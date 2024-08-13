3b3e0339ed932159fb7cc7deb0b0780d2530550f
RBDevScripts <itsritbis@gmail.com>
2024-08-13 20:07:43 +0530

    Added AI functionality

--- projectname/Airecipe/urls.py
+++ projectname/Airecipe/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from .views import generate_recipe
+
 
 urlpatterns = [
     path('',views.home2, name='index-page'),
     path('generate/',views.generate,name="generate-page"),
     path('wishlist/',views.wishlist,name="wishlist-page"),
+    path('generate-recipe/', generate_recipe, name='generate_recipe'),
+    
 ]
--- projectname/Airecipe/views.py
+++ projectname/Airecipe/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from groq import Groq
+from django.http import JsonResponse
+
+from django.views.decorators.csrf import csrf_exempt
+import json
+
 
 # Create your views here.
 def home2(request):
@@ -7,3 +13,36 @@ def generate(request):
     return render(request,"generate.html")
 def wishlist(request):
     return render(request,"wishlist.html")
+
+def recip(itemss):
+    client = Groq(api_key="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")
+
+    promt = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {
+                "role": "system",
+                "content": "you are a recipe generator from the given ingridents only"
+            },
+            {
+                "role": "user",
+                "content": f"Generate a recipe using the following ingredients: {itemss}"
+            }
+        ],
+        
+    )
+    
+    workout_plan = promt.choices[0].message.content
+    return workout_plan
+
+@csrf_exempt
+def generate_recipe(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        items = data.get('items')
+
+        
+        recipe = recip(items)
+
+        return JsonResponse({'recipe': recipe})
+    return JsonResponse({'error': 'Invalid request'}, status=400)
